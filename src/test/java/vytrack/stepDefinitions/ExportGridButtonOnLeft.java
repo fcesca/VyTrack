cc04153e866524c2c5e4bfababaaa8dfd7ce2f69
sabiter <sabiter1134@gmail.com>
2022-07-20 17:10:08 -0400

    features and step definition for 2nd AC

---
+++ src/test/java/vytrack/stepDefinitions/ExportGridButtonOnLeft.java
@@ -0,0 +1,15 @@
+package vytrack.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class ExportGridButtonOnLeft {
+
+    @Given("that the user is on the Vehicles Page under Fleet Module")
+    public void that_the_user_is_on_the_vehicles_page_under_fleet_module() {
+
+    }
+    @Given("the user should see the Export Grid dropdown on the left of the page")
+    public void the_user_should_see_the_export_grid_dropdown_on_the_left_of_the_page() {
+
+    }
+}
